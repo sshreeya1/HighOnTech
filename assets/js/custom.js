290d13ba368326d76e98fb417013860d3eb462fc
Bikesh <bikesh17stha@gmail.com>
2022-07-13 16:58:52 +0545

    Chat feature for all pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -181,3 +181,13 @@
 
 
 })(window.jQuery);
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/62ce97dd7b967b1179994f4a/1g7rg2emr';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
